0ed5ded1e8977b5e602da095705238a21677eb85
Mic-guel <katilimicguel@gmail.com>
2026-09-13 11:33:30 +0700

    Defining Project class and migrating

--- main/models.py
+++ main/models.py
@@ -1,6 +1,13 @@
 import uuid
 from django.db import models
 
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=20)
+    date = models.DateField(blank=True, default="")
+
 class Experience(models.Model):
     EXPERIENCE_CHOICES = [
         ('internship', 'Internship'),
@@ -17,7 +24,7 @@ class Experience(models.Model):
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
     thumbnail = models.URLField(blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
-    ended_at = models.DateTimeField(blank=True, null=True)
+    ended_at = models.DateTimeField(blank=True, default="")
     def __str__(self):
         return self.title
     
